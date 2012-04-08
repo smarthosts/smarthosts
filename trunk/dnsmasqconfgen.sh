8e0c05a586a2c3da7d9ff6c57bdaa4a48319a6fc
liruqi <liruqi@cc4120ec-e825-89e5-66e4-b718fe11252a>
2012-04-08 05:04:01 +0000

    restore dnsmasq server config
    git-svn-id: http://smarthosts.googlecode.com/svn@206 cc4120ec-e825-89e5-66e4-b718fe11252a

--- trunk/dnsmasqconfgen.sh
+++ trunk/dnsmasqconfgen.sh
@@ -1,10 +1,13 @@
 echo "#Author: liruqi@gmail.com"
 echo "#Date: `date`"
-echo "#Description: generate dnsmasq conf from hosts file"
-cat hosts| egrep -v "^#|^$|localhost|0\.0\.0\.0|wiki|googleusercontent"|awk '{print "address=/"$2"/"$1; }'
+echo "#Description: generate dnsmasq conf from hosts file\n"
 
 echo $(cat <<EOF
-
+server=/android.clients.google.com/114.114.114.114
+server=/top100.cn/114.114.114.114
+server=/mtalk.google.com/8.8.4.4
+server=/talk.google.com/8.8.4.4
+server=/reader.googleusercontent.com/8.8.4.4
 #tumblr
 alias=174.121.98.156,174.121.194.34 
 alias=50.22.53.157,174.121.194.34 
@@ -16,3 +19,6 @@ alias=107.20.170.126,204.236.224.226
 alias=174.129.199.91,204.236.224.226
 EOF
 ) | awk 'BEGIN { RS = " " } ; { print $0 }'
+cat hosts| egrep -v "^#|^$|localhost|0\.0\.0\.0|wiki|googleusercontent"|awk '{print "address=/"$2"/"$1; }'
+
+
