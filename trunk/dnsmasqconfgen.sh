63a74b6e4c82a71af5584bd86d35e9552151a0fd
liruqi <liruqi@cc4120ec-e825-89e5-66e4-b718fe11252a>
2012-10-13 05:07:30 +0000

    dropbox unblocked
    git-svn-id: http://smarthosts.googlecode.com/svn@256 cc4120ec-e825-89e5-66e4-b718fe11252a

--- trunk/dnsmasqconfgen.sh
+++ trunk/dnsmasqconfgen.sh
@@ -15,11 +15,8 @@ alias=50.22.53.155,66.6.32.137
 alias=72.32.231.8,66.6.32.137
 alias=174.121.66.230,66.6.32.137
 alias=174.121.194.34,66.6.32.137
-#dropbox
-alias=107.20.170.126,204.236.224.226
-alias=174.129.199.91,204.236.224.226
 EOF
 ) | awk 'BEGIN { RS = " " } ; { print $0 }'
-cat hosts| egrep -v "^#|^$|localhost|0\.0\.0\.0|wiki|googleusercontent"|awk '{print "address=/"$2"/"$1; }'
+cat hosts| egrep -v "^#|^$|localhost|dropbox|0\.0\.0\.0|wiki|googleusercontent"|awk '{print "address=/"$2"/"$1; }'
 
 
